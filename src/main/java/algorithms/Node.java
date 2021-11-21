3afb08ea72e1e85b9ee35c454661e9499ab74ae8
Youssef <Youssef@192.168.1.5>
2021-11-21 10:06:16 +0200

    node class

---
+++ src/main/java/algorithms/Node.java
@@ -0,0 +1,15 @@
+package algorithms;
+
+import java.util.List;
+
+public class Node {
+	public int minmax;
+	public int alpha;
+	public int betta;
+	public Node parent ;
+	public List <Node> childern;
+
+	public int position_x;
+	public int position_y;
+	
+}
