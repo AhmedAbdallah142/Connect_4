dea30e792001f492fb63a754ffffb8a29b33d415
Youssef <Youssef@192.168.1.5>
2021-11-22 21:30:42 +0200

    graph fater draw

--- src/main/java/algorithms/Node.java
+++ src/main/java/algorithms/Node.java
@@ -3,18 +3,27 @@ package algorithms;
 import java.util.LinkedList;
 import java.util.List;
 
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Line;
+
 public class Node {
 	public int minmax;
 	public int alpha;
 	public int beta;
+    public int level=0;
 	public Node parent;
 	public List <Node> childs = new LinkedList<>();
+	public StackPane value ;
 
-	public int position_x;
-	public int position_y;
 
 	public Node (Node parent) {
 		this.parent = parent;
+        if(parent==null){
+            level=0;
+        }
+        else{
+          level=parent.level+1;  
+        } 
 	}
 
 	public void setValues (int minmax, int alpha, int beta) {
@@ -22,5 +31,26 @@ public class Node {
 		this.alpha = alpha;
 		this.beta = beta;
 	}
+
+	public Line add_line(StackPane s){
+        if(parent!=null){
+            int index=-1;
+            int i=0;
+            for(i=0 ;i< parent.childs.size() ;i++){   
+                if (parent.childs.get(i).value == s){
+                    index=i;
+                    break;
+                }
+            }
+            if(index!=-1){
+                Line line = new Line(parent.value.getTranslateX()+50, parent.value.getTranslateY()+2*50,value.getTranslateX()+50, value.getTranslateY());
+                //lines_of_children.add(line);
+                return line; 
+            }
+           
+        }
+        return null;
+        
+    }
 	
 }
--- src/main/java/app/connect_4/HelloApplication.java
+++ src/main/java/app/connect_4/HelloApplication.java
@@ -3,14 +3,10 @@ package app.connect_4;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Vector;
 
 import algorithms.Node;
-import javafx.application.Application;
 import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -19,7 +15,6 @@ import javafx.scene.shape.StrokeType;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextBoundsType;
-import javafx.stage.Stage;
 
 public class HelloApplication {
     private static  int k_level;
@@ -32,65 +27,44 @@ public class HelloApplication {
     private static final int R = 50;
     private static Group group_gl =new Group();
 
-    public static void printInorder(tree s,Node node)
-    {
-        if (node == null) {
-            return;
-        }
-        for(int i=0 ;i< node.childs.size() ;i++){
-           tree child = new tree(create_circle_position(),s);
-           s.childern.add(child);
-           printInorder(child,node.childs.get(i));
-        }
-    }
-
-    public static void printInorder(tree s)
-    {
-        if (s == null) {
-            return;
-        }
-        System.out.println(s.t);
-        group_gl.getChildren().add(s.value);
-        for(int i=0 ;i< s.childern.size() ;i++){    
-            printInorder(s.childern.get(i));
-        }
-    }
-    public void countNode(tree root){
-        if(root==null)
-            return ;
-        V++;
-        for(int i=0 ;i< root.childern.size() ;i++){    
-            countNode(root.childern.get(i));
-        }
-    }
-    public int getLevelUtil(tree node, BorderPane data, int level)
-    {
-        if (node == null)
-            return 0;
+    // public void countNode(tree root){
+    //     if(root==null)
+    //         return ;
+    //     V++;
+    //     for(int i=0 ;i< root.childern.size() ;i++){    
+    //         countNode(root.childern.get(i));
+    //     }
+    // }
+    // public int getLevelUtil(Node node, StackPane data, int level)
+    // {
+    //     if (node == null)
+    //         return 0;
  
-        if (node.value == data)
-            return level;
-        int downlevel=0;
-        for(int i=0 ;i< node.childern.size() ;i++){   
-            downlevel +=getLevelUtil(node.childern.get(i), data, level + 1);
-        }
-        return downlevel;
-    }
-    int getLevel(tree node, BorderPane data)
-    {
-        return getLevelUtil(node, data, 0);
-    }
+    //     if (node.value == data)
+    //         return level;
+    //     int downlevel=0;
+    //     for(int i=0 ;i< node.childs.size() ;i++){   
+    //         downlevel +=getLevelUtil(node.childs.get(i), data, level + 1);
+    //     }
+    //     return downlevel;
+    // }
+    // int getLevel(Node node, StackPane value)
+    // {
+    //     return getLevelUtil(node, value, 0);
+    // }
 
-    private void Bfs(tree goal) {
-		HashMap<tree, tree> seen = new HashMap<tree, tree>();
-		Queue<tree> frontier = new LinkedList<tree>();
+    private void Bfs(Node goal) {
+		HashMap<Node, Node> seen = new HashMap<Node, Node>();
+		Queue<Node> frontier = new LinkedList<Node>();
 		
 		seen.put(goal, null);
 		frontier.add(goal);
 		
 		while (!frontier.isEmpty()) {
-			tree state = frontier.remove();
-            int level=	getLevel(goal,state.value);
+			Node state = frontier.remove();
+            state.value=create_circle_position();
+            group_gl.getChildren().add(state.value);
+            int level=	state.level;
             if(levet_visited[level]==0){
                 state.value.setTranslateX(0);
                 height+=200;
@@ -105,7 +79,7 @@ public class HelloApplication {
             if(l!=null){
                 group_gl.getChildren().add(l);
             }
-			for (tree move : state.childern) {
+			for (Node move : state.childs) {
 				if (!seen.containsKey(move)) {
 					seen.put(move, state);
 					frontier.add(move);
@@ -129,11 +103,7 @@ public class HelloApplication {
 
         ScrollPane scroll = new ScrollPane();
         scroll.setPrefSize(700, 700);
-        tree root_tree = new tree(create_circle_position(),null);
-
-        printInorder(root_tree,root);
-        printInorder(root_tree);
-        Bfs(root_tree);
+        Bfs(root);
 
         StackPane stack = new StackPane();
         stack.getChildren().addAll(group_gl);
@@ -142,45 +112,41 @@ public class HelloApplication {
 
     }
 
-    /*@Override
-    public void start(final Stage stage) throws Exception {
-        Node root=new Node(null);
-        Node n1=new Node(root);
-        Node n2=new Node(root);
-        Node n3=new Node(n1);
-        Node n4=new Node(n2);
-        Node n5=new Node(n3);
-        Node n6=new Node(n2);
-        Node n7=new Node(n1);
+    // @Override
+    // public void start(final Stage stage) throws Exception {
+    //     Node root=new Node(null);
+    //     Node n1=new Node(root);
+    //     Node n2=new Node(root);
+    //     Node n3=new Node(n1);
+    //     Node n4=new Node(n2);
+    //     Node n5=new Node(n3);
+    //     Node n6=new Node(n2);
+    //     Node n7=new Node(n1);
 
-        root.childs.add(n1);
-        root.childs.add(n2);
-        n1.childs.add(n3);
-        n1.childs.add(n7);
-        n2.childs.add(n4);
-        n2.childs.add(n6);
-        n3.childs.add(n5);
+    //     root.childs.add(n1);
+    //     root.childs.add(n2);
+    //     n1.childs.add(n3);
+    //     n1.childs.add(n7);
+    //     n2.childs.add(n4);
+    //     n2.childs.add(n6);
+    //     n3.childs.add(n5);
 
-        ScrollPane scroll = new ScrollPane();
-        scroll=draw_graph(root,10);
-        stage.setScene(new Scene(scroll));
-        stage.show();
+    //     ScrollPane scroll = new ScrollPane();
+    //     scroll=draw_graph(root,10);
+    //     stage.setScene(new Scene(scroll));
+    //     stage.show();
 
-    }*/
+    // }
 
-    private static BorderPane create_circle_position(){  
+    private static StackPane create_circle_position(){  
 
         final Circle circle = createCircle();
         final Text   text   = createText();
 
-        Group group = new Group(circle);
-            
         StackPane stack = new StackPane();
-        stack.getChildren().addAll(group,text);
+        stack.getChildren().addAll(circle,text);
 
-        BorderPane border = new BorderPane();
-        border.setCenter(stack);
-        return border;
+        return stack;
     }
 
     private static Circle createCircle() {
@@ -197,7 +163,6 @@ public class HelloApplication {
 
     private static Text createText() {
         final Text text = new Text("A");
-
         text.setFont(new Font(30));
         text.setBoundsType(TextBoundsType.VISUAL);
 
